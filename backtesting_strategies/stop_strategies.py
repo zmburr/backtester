4b1fe7706cb9ea232822d463750c152d5618f54d
zmbur <zmburr@gmail.com>
2024-01-30 14:43:46 -0500

    small changes

--- backtesting_strategies/stop_strategies.py
+++ backtesting_strategies/stop_strategies.py
@@ -9,20 +9,27 @@ class stopStrategies:
 
     def high_of_day(self):
         df = self.data.between_time('06:00:00', self.search_time)
-        self.trade.stop_price = df.high.max()
-        self.trade.stop_strategy = 'high_of_day'
+        return df.high.max()
 
     def low_of_day(self):
         df = self.data.between_time('06:00:00', self.search_time)
         return df.low.min()
 
     def premarket_high(self):
-        df = self.data.between_time('06:00:00', self.search_time)
-        return df.between_time('06:00:00', '09:29:59').high.max()
+        df = self.data.between_time('06:00:00', '09:29:59')
+        return df.high.max()
 
     def premarket_low(self):
-        df = self.data.between_time('06:00:00', self.search_time)
-        return df.between_time('06:00:00', '09:29:59').low.min()
+        df = self.data.between_time('06:00:00', '09:29:59')
+        return df.low.min()
 
     def open_price(self):
         return self.data.between_time('09:30:00', '09:32:00').iloc[0].open
+
+    def set_stop_price(self):
+        strategy_method = getattr(self, self.trade.stop_strategy, None)
+        if callable(strategy_method):
+            self.trade.stop_price = strategy_method()
+        else:
+            print(f"Unknown strategy: {self.trade.stop_strategy}")
+
