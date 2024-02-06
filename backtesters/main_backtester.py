29f5c5c218162aa1810f4f5388dd2bca91f64acc
zmbur <zmburr@gmail.com>
2024-02-06 14:10:50 -0500

    combined reversal and momo backtester

--- backtesters/main_backtester.py
+++ backtesters/main_backtester.py
@@ -7,21 +7,26 @@ from backtesting_strategies.entry_signals import entrySignals
 from backtesting_strategies.stop_strategies import stopStrategies
 from backtesting_strategies.exit_signals import exitSignals
 import logging
-from data_collectors.momentum_data_collection import df
+from data_collectors.combined_data_collection import momentum_df
+from data_collectors.combined_data_collection import reversal_df
 from tabulate import tabulate
 
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+trade_type = 'reversal'
+
 trades = []
+
 if __name__ == '__main__':
+    df = reversal_df if trade_type == 'reversal' else momentum_df
     for index, row in df.iterrows():
         ticker = row['ticker']
         wrong_date = datetime.strptime(row['date'], '%m/%d/%Y')
         date = wrong_date.strftime('%Y-%m-%d')
         logging.info(f'Processing trade for {ticker} on {date}')
-        trade = backtestTrade(date, ticker, 'BUY')
+        trade = backtestTrade(date, ticker, 'SELL') if trade_type == 'reversal' else backtestTrade(date, ticker, 'BUY')
         entry = entrySignals(trade)
         trade.determine_signal()
         stop = stopStrategies(trade)
@@ -39,4 +44,4 @@ if __name__ == '__main__':
     trades_df.drop('recommendation', axis=1, inplace=True)
     trades_df.drop('side', axis=1, inplace=True)
     print(tabulate(trades_df, headers=trades_df.columns))
-    trades_df.to_csv(f'C:\\Users\\zmbur\\PycharmProjects\\backtester\\data\\momentum_backtest_results.csv')
+    trades_df.to_csv(f'C:\\Users\\zmbur\\PycharmProjects\\backtester\\data\\{trade_type}_backtest_results.csv')
